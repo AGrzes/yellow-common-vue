067873ab7115773b0b94f344478dc8062e82c33a
Grzegorz Abramczyk <abramczykg@gmail.com>
2019-10-17 20:57:29 +0200

    Packaged bootstrap

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ export * from './routes'
 export * from './api'
 import Vue from 'vue'
 export {Vue}
+import VueRourter from 'vue-router'
+export {VueRourter}
+
+export const bootstrap = () => {
+  import('./bootstrap')
+}
