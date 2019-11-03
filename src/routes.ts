f46573eecfe83d9f3787629753c14da49fda4166
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2019-11-03 08:51:14 +0100

    Updated router generator to use class for common configuration

--- src/routes.ts
+++ src/routes.ts
@@ -14,79 +14,94 @@ export const handleRoute = (handler: (vm, route) => void) => ({
 }
 )
 
-interface ListHandlerOptions {
-  name: string
-  listName?: string
+interface HandlerOptions {
   containerWrapper?(content: string): string
+  listFetchGenerator?(name: string): () => Promise<any[]>
+  itemFetchGenerator?(name: string): (id: string) => Promise<any>
+  itemProperty?(name: string): string
+  listControl?(name: string): string
+  listPath?(name: string): string
+  itemPath?(name: string): string
+  itemControl?(name: string): string
+}
+interface ListHandlerOptions  {
+  name: string
+  path?: string
   control?: string
   itemProperty?: string
   fetch?(): Promise<any[]>
-  fetchGenerator?(): () => Promise<any[]>
-}
-
-export const listHandler = (options: ListHandlerOptions): RouteConfig => {
-  const name = options.name
-  const listName = options.listName || `${name}s`
-  const containerWrapper = options.containerWrapper || ((content) => `
-  <div class="container mt-3">
-    ${content}
-  </div>
-        `)
-  const control = options.control || `${name}-summary`
-  const itemProperty = options.itemProperty || name
-  const fetch = options.fetch || options.fetchGenerator()
-  return {
-    name: `${name}-list`,
-    path: `${listName}`,
-    component: Vue.extend({
-      template: containerWrapper(`<${control} v-for="${name} in ${listName}" :${itemProperty}="${name}"></${control}>`),
-        data() {
-          return {
-            [listName]: []
-          }
-        },
-        mounted() {
-          fetch().then((data) => this[listName] = data)
-        }
-    })
-  }
+  containerWrapper?(content: string): string
 }
 
 interface ItemHandlerOptions {
   name: string
-  listName?: string
+  path?: string
   containerWrapper?(content: string): string
   control?: string
   itemProperty?: string
   fetch?(id: string): Promise<any>
-  fetchGenerator?(): (id: string) => Promise<any>
 }
 
-export const itemHandler = (options: ItemHandlerOptions): RouteConfig => {
-  const name = options.name
-  const listName = options.listName || `${name}s`
-  const containerWrapper = options.containerWrapper || ((content) => `
-  <div class="container mt-3">
-    ${content}
-  </div>
-        `)
-  const control = options.control || `${name}-details`
-  const itemProperty = options.itemProperty || name
-  const fetch = options.fetch || options.fetchGenerator()
-  return {
-    name,
-    path: `${listName}/:${name}`,
-    component: Vue.extend({
-      template: containerWrapper(`<${control} :${itemProperty}="${name}" v-if="${name}"></${control}>`),
-        data() {
-          return {
-            [name]: null
-          }
-        },
-        ...handleRoute((vm, route) => {
-          fetch(route.params[name]).then((data) => vm[name] = data)
-        })
+export class HandlerGenerator {
+  constructor(private options: HandlerOptions) {
+    this.options.listFetchGenerator = this.options.listFetchGenerator || (() => {
+      throw new Error('Fetch not defined')
     })
+    this.options.containerWrapper = this.options.containerWrapper || ((content) => content)
+    this.options.itemProperty = this.options.itemProperty || ((name) => name)
+    this.options.listControl = this.options.listControl || ((name) => `${name}-summary`)
+    this.options.listPath = this.options.listPath || ((name) => `${name}`)
+    this.options.itemPath = this.options.itemPath || ((name) => `${this.options.listPath(name)}/:${this.options.itemProperty(name)}`)
+    this.options.itemControl = this.options.itemControl || ((name) => `${name}-details`)
+  }
+
+  public list(options: ListHandlerOptions): RouteConfig {
+    const name = options.name
+    const path = options.path || this.options.listPath(name)
+    const containerWrapper = options.containerWrapper || this.options.containerWrapper
+    const control = options.control || this.options.listControl(name)
+    const itemProperty = options.itemProperty || this.options.itemProperty(name)
+    const fetch = options.fetch || this.options.listFetchGenerator(name)
+    return {
+      name: `${name}-list`,
+      path,
+      component: Vue.extend({
+        template: containerWrapper(`
+<${control} v-for="item in list" :${itemProperty}="item"></${control}>
+        `),
+          data() {
+            return {
+              list: []
+            }
+          },
+          mounted() {
+            fetch().then((data) => this.list = data)
+          }
+      })
+    }
+  }
+  public item(options: ItemHandlerOptions): RouteConfig {
+    const name = options.name
+    const path = options.path || this.options.itemPath(name)
+    const containerWrapper = options.containerWrapper || this.options.containerWrapper
+    const control = options.control || this.options.itemControl(name)
+    const itemProperty = options.itemProperty || this.options.itemProperty(name)
+    const fetch = options.fetch || this.options.itemFetchGenerator(name)
+    return {
+      name,
+      path,
+      component: Vue.extend({
+        template: containerWrapper(`<${control} :${itemProperty}="item" v-if="item"></${control}>`),
+          data() {
+            return {
+              item: null
+            }
+          },
+          ...handleRoute((vm, route) => {
+            fetch(route.params[name]).then((data) => vm.item = data)
+          })
+      })
+    }
   }
 }
 
